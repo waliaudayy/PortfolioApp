80496e7ad535b17d82bc7f4b59cc4fb518c36a79
uday walia <waliaudayy23@gmail.com>
2025-07-02 22:18:44 +0530

    1st update

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Portfolio from "@/component/portfolio";
+import Portfolio from "@/component/Portfolio";
 
 export default function Home() {
   return <Portfolio />;
